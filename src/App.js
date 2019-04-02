497343de8a9ff9e50429ce0e20190d8a7213d6d2
Noah A <nanderson815@gmail.com>
2019-04-02 18:44:39 -0400

    first commit;

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
 
-class App extends Component {
+
+class App extends React.Component {
+  state = {
+    friends: friends
+  }
+
+  handleDelete = id => {
+    console.log(id)
+    const friends = this.state.friends.filter(friend => friend.id !== id)
+    this.setState({ friends });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Wrapper>
+        <h1 className="title">Friends List</h1>
+        {this.state.friends.map((data) => <FriendCard delete={this.handleDelete} key={data.id} data={data} />)}
+      </Wrapper>
+    )
   }
+
 }
 
 export default App;
