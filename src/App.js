f53fee079c673a7538a6781cf699c7ccf08e6c1b
Noah A <nanderson815@gmail.com>
2019-04-02 20:26:46 -0400

    new activity

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import friends from "./friends.json";
-import "./App.css";
+import Form from "./components/Form";
 
-
-class App extends React.Component {
-  state = {
-    friends: friends
-  }
-
-  handleDelete = id => {
-    console.log(id)
-    const friends = this.state.friends.filter(friend => friend.id !== id)
-    this.setState({ friends });
-  }
-
-  render() {
-    return (
-      <Wrapper>
-        <h1 className="title">Friends List</h1>
-        {this.state.friends.map((data) => <FriendCard delete={this.handleDelete} key={data.id} data={data} />)}
-      </Wrapper>
-    )
-  }
-
+function App() {
+  return <Form />;
 }
 
 export default App;
